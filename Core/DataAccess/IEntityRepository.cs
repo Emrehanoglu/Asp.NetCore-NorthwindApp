9c564f8ab67af01bebab5658b04ce6359fa69642
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-01-12 23:59:09 +0300

    NorthwindApp #3

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{
+	public interface IEntityRepository<T> where T : class, IEntity, new()
+	{
+		T Get(Expression<Func<T, bool>> filter);
+	}
+}
