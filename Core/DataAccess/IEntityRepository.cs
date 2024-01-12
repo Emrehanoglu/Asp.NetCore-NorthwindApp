ad0141c9633ed3308b6070162363b0e55ad2d2c2
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-01-13 00:11:30 +0300

    NorthwindApp #4

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -9,5 +9,9 @@ namespace Core.DataAccess
 	public interface IEntityRepository<T> where T : class, IEntity, new()
 	{
 		T Get(Expression<Func<T, bool>> filter);
+		IList<T> GetList(Expression<Func<T, bool>> filter = null);
+		void Add(T entity);
+		void Update(T entity);
+		void Delete(T entity);
 	}
 }
