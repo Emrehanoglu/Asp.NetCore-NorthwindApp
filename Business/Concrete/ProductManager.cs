85ceb28b7c595b9facecbd799bd2f031c0924db6
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-01-13 11:08:45 +0300

    NorthwindApp #7

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,41 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+	public class ProductManager : IProductService
+	{
+		public void Add(Product product)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete(Product product)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Product GetById(int productId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Product> GetList()
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Product> GetListByCategory(int categoryId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(Product product)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
