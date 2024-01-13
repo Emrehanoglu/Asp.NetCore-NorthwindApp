74148131565957bfd920f2b6a3cfbc3c4c8b27ca
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-01-13 12:07:52 +0300

    NorthwindApp #11

---
+++ Core/Utilities/Results/ErrorDataResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+	public class ErrorDataResult<T>:DataResult<T>
+	{
+		public ErrorDataResult(T data, string message) : base(data, false, message) { }
+		public ErrorDataResult(T data) : base(data, false) { }
+
+		//aşağıdakileri proje boyunca kullanmayacağım ama kullanım şekli olara data'nın
+		//gelmediği durumlarda default yapısı bu şekilde kullanılıyor.
+		//sırasıyla sadece durum ve sadece message gectıgım metotlar
+		public ErrorDataResult() : base(default, false) { }
+		public ErrorDataResult(string message) : base(default, false, message) { }
+	}
+}
