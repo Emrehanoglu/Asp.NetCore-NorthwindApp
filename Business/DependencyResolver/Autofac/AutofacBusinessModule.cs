6c02ee9da37ca07ff6b50f92d2edc61e74e1f0b7
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-01-14 00:33:15 +0300

    NorthwindApp #14

---
+++ Business/DependencyResolver/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolver.Autofac
+{
+	public class AutofacBusinessModule:Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterType<ProductManager>().As<IProductService>(); // IProductService istenirse ona ProductManager verilecek.
+			builder.RegisterType<EfProductDal>().As<IProductDal>(); // IProductDal istenirse ona EfProductDal verilecek.
+		}
+	}
+}
