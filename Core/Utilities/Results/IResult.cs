64268ea4d42f62c9b773fbcbd43bca711063d891
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-01-13 11:53:09 +0300

    NorthwindApp #9

---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+	public interface IResult
+	{
+		bool Success { get; } //işlem başarılı mı, burası sadece readonly olacak
+		string Message { get; } // işlemin acıklaması
+	}
+}
