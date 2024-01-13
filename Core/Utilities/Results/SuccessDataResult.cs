e1481bd7dc88257eebd64f639c9cd5a4efdcb03f
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-01-13 12:05:51 +0300

    NorthwindApp #10

---
+++ Core/Utilities/Results/SuccessDataResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+	public class SuccessDataResult<T>:DataResult<T>
+	{
+		public SuccessDataResult(T data, string message) : base(data, true, message) { }
+		public SuccessDataResult(T data) : base(data, true) { }
+		
+		//aşağıdakileri proje boyunca kullanmayacağım ama kullanım şekli olara data'nın
+		//gelmediği durumlarda default yapısı bu şekilde kullanılıyor.
+		//sırasıyla sadece durum ve sadece message gectıgım metotlar
+		public SuccessDataResult() : base(default, true) { }
+		public SuccessDataResult(string message) : base(default, true, message) { }
+	}
+}
