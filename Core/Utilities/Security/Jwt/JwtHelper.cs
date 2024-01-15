83831aecc235d88d77e6de6ca86fd4b693a590ad
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-01-15 23:52:06 +0300

    NorthwindApp #24

--- Core/Utilities/Security/Jwt/JwtHelper.cs
+++ Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -1,8 +1,11 @@
 ﻿using Core.Entities.Concrete;
 using Core.Utilities.Security.Encryption;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace Core.Utilities.Security.Jwt
@@ -11,15 +14,39 @@ namespace Core.Utilities.Security.Jwt
 	{
 		public IConfiguration Configuration { get; }
 		private TokenOptions _tokenOptions;
-		public JwtHelper(IConfiguration configuration, TokenOptions tokenOptions)
+		private DateTime _accessTokenExpiration;
+		public JwtHelper(IConfiguration configuration, TokenOptions tokenOptions, DateTime accessTokenExpiration)
 		{
 			Configuration = configuration;
 			_tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+			_accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOptions.AccessTokenExpiration);
 		}
 
 		public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
 		{
 			var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
+			var signingCredentials = SigningCredentialsHelper.CreateSigningCredentials(securityKey);
+			return default;
+		}
+
+		public JwtSecurityToken CreateJwtSecurityToken(TokenOptions tokenOptions, User user, 
+			SigningCredentials signingCredentials, List<OperationClaim> operationClaims)
+		{
+			var jwt = new JwtSecurityToken(
+				issuer:tokenOptions.Issuer,
+				audience:tokenOptions.Audience,
+				expires: _accessTokenExpiration,
+				notBefore:DateTime.Now,
+				claims:operationClaims,
+				signingCredentials:signingCredentials
+				);
+		}
+
+		//claim olarak set edeceğim bir metot
+		private IEnumerable<Claim> SetClaims(User user, List<OperationClaim> operationClaims)
+		{
+			var claims = new List<Claim>();
+			claims.Add(new Claim("email",user.Email));
 		}
 	}
 }
