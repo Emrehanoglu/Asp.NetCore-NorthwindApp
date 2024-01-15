1fa814e2a985824d400cabefc283bdcc9306f3f3
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-01-15 22:31:23 +0300

    NorthwindApp #22

---
+++ Core/Utilities/Security/Jwt/TokenOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Jwt
+{
+	public class TokenOptions
+	{
+		public string Audience { get; set; } //token 'ın kullanıcı kitlesi
+		public string Issuer { get; set; } //token 'ı imzalayan
+		public int AccessTokenExpiration { get; set; } //dk cinsinden token'ın gecerlilik süresi
+		public string SecurityKey { get; set; }
+	}
+}
