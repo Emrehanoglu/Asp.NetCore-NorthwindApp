f14b8ffd5ee7abcb4c24285a690d6c1632d273ee
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-01-15 23:03:34 +0300

    NorthwindApp #23

--- Core/Utilities/Security/Jwt/JwtHelper.cs
+++ Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -1,4 +1,6 @@
 ﻿using Core.Entities.Concrete;
+using Core.Utilities.Security.Encryption;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,9 +9,17 @@ namespace Core.Utilities.Security.Jwt
 {
 	public class JwtHelper : ITokenHelper
 	{
+		public IConfiguration Configuration { get; }
+		private TokenOptions _tokenOptions;
+		public JwtHelper(IConfiguration configuration, TokenOptions tokenOptions)
+		{
+			Configuration = configuration;
+			_tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+		}
+
 		public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
 		{
-			throw new NotImplementedException();
+			var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
 		}
 	}
 }
