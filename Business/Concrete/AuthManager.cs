a51f58ce4f3caad2a6e9c8d9566f75fa6882b9ff
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-01-16 21:47:59 +0300

    NorthwindApp #26

---
+++ Business/Concrete/AuthManager.cs
@@ -0,0 +1,42 @@
+﻿using Business.Abstract;
+using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using Core.Utilities.Security.Jwt;
+using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+	public class AuthManager : IAuthService
+	{
+		private IUserService _userService;
+		private ITokenHelper _tokenHelper;
+		public AuthManager(IUserService userService, ITokenHelper tokenHelper)
+		{
+			_userService = userService;
+			_tokenHelper = tokenHelper;
+		}
+
+		public IDataResult<AccessToken> CreateAccessToken(User user)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IDataResult<User> Login(UserForLoginDto userForLoginDto)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IResult UserExist(string email)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
