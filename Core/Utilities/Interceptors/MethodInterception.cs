a3009ec16eaf7dc6b9bae029d9b1c392f89fb797
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-01-22 21:02:01 +0300

    NorthwindApp #37

---
+++ Core/Utilities/Interceptors/MethodInterception.cs
@@ -0,0 +1,40 @@
+﻿using Castle.Core.Interceptor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Interceptors
+{
+	//bir metodun nerede nasıl yorumlanacağın bilineceği yer olacak
+	public abstract class MethodInterception: MethodInterceptionBaseAttribute
+	{
+		protected virtual void OnBefore(IInvocation ınvocation) { } //metot çalışmadan once calısacak yer
+		protected virtual void OnAfter(IInvocation ınvocation) { } //metot calıstıktan sonra calısacak yer
+		protected virtual void OnException(IInvocation ınvocation) { } //metot hata verirse calısacak yer
+		protected virtual void OnSuccess(IInvocation ınvocation) { } //metot başarılı calısırsa calısacak yer
+		
+		public override void Intercept(IInvocation invocation)
+		{
+			var isSuccess = true;
+			OnBefore(invocation);
+			try
+			{
+				invocation.Proceed();
+			}
+			catch (Exception)
+			{
+				isSuccess = false;
+				OnException(invocation);
+				throw;
+			}
+			finally
+			{
+				if (isSuccess)
+				{
+					OnSuccess(invocation);
+				}
+			}
+			OnAfter(invocation);
+		}
+	}
+}
--- Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
+++ Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using Castle.Core.Interceptor;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Core.Utilities.Interceptors
 {
-	public abstract class MethodInterceptionBaseAttribute:Attribute,IInterceptor
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple =true, Inherited =true)]
+	public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
 	{
+		public int Priority { get; set; }
+
+		//metot farklı aspectlere hizmet edeceği için virtual olmalı
+		public virtual void Intercept(IInvocation invocation)
+		{
+			//içi bos kalacak
+		}
 	}
 }
