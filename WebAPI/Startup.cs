1d1fc7223e168f0f24011e85e807608ecb0c4e50
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-01-23 21:14:19 +0300

    NorthwindApp #47

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,3 +1,6 @@
+using Core.DependencyResolver;
+using Core.Extensions;
+using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.Jwt;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -51,6 +54,11 @@ namespace WebAPI
 						IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
 					};
 				});
+
+			services.AddDependencyResolvers(new ICoreModule[]
+			{
+				new CoreModule()
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
