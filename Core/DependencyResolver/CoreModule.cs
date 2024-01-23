04ffc9be02368e56a7fef912ff1c44dfb3118aa7
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-01-23 20:59:46 +0300

    NorthwindApp #45

---
+++ Core/DependencyResolver/CoreModule.cs
@@ -0,0 +1,16 @@
+﻿using Core.Utilities.IoC;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DependencyResolver
+{
+	public class CoreModule : ICoreModule
+	{
+		public void Load(IServiceCollection collection)
+		{
+			collection.AddMemoryCache();
+		}
+	}
+}
