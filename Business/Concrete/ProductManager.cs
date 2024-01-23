7dc95676e8ee3eca562607b25e8ed735f9ae48ab
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-01-23 20:34:43 +0300

    NorthwindApp #42

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Transaction;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
@@ -50,6 +51,13 @@ namespace Business.Concrete
 		public IDataResult<List<Product>> GetListByCategory(int categoryId)
 		{
 			return new SuccessDataResult<List<Product>>(_productDal.GetList(x => x.CategoryId == categoryId).ToList());
+		}
+		[TransactionScopeAspect]
+		public IResult TransactionalOperation(Product product)
+		{
+			_productDal.Update(product);
+			_productDal.Add(product);
+			return new SuccessResult(Messages.ProductUpdated);
 		}
 
 		public IResult Update(Product product)
--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -81,6 +81,18 @@ namespace WebAPI.Controllers
 			return BadRequest(result.Message);
 		}
 
+		[HttpPost("transaction")]
+		public IActionResult TransactionTest(Product product)
+		{
+			var result = _productService.TransactionalOperation(product);
+			if (result.Success)
+			{
+				return Ok(result.Message);
+			}
+
+			return BadRequest(result.Message);
+		}
+
 		[HttpPost("update")]
 		public IActionResult Update(Product product)
 		{
