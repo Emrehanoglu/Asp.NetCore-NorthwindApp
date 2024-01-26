1a91313cfcb194941df4aef7c668c54840ecda10
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-01-26 22:51:58 +0300

    NorthwindApp #56

--- Core/CrossCuttingConcerns/Logging/Log4Net/Layouts/JsonLayout.cs
+++ Core/CrossCuttingConcerns/Logging/Log4Net/Layouts/JsonLayout.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using log4net.Core;
+using log4net.Layout;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Core.CrossCuttingConcerns.Logging.Log4Net.Layouts
 {
-	public class JsonLayout
+	public class JsonLayout : LayoutSkeleton
 	{
+		public override void ActivateOptions()
+		{
+
+		}
+
+		public override void Format(TextWriter writer, LoggingEvent loggingEvent)
+		{
+			var logEvent = new SerializableLogEvent(loggingEvent);
+			var json = JsonConvert.SerializeObject(logEvent, Formatting.Indented);
+			writer.WriteLine(json);
+		}
 	}
 }
