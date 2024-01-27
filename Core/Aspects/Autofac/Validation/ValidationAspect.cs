373ab8c508847fd5408de0865bbfa0592c026643
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-01-27 16:36:08 +0300

    NorthwindApp #65

--- Core/Aspects/Autofac/Validation/ValidationAspect.cs
+++ Core/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -18,7 +18,7 @@ namespace Core.Aspects.Autofac.Validation
 			if (!typeof(IValidator).IsAssignableFrom(validatorType))
 			{
 				//gonderilen validatorType, IValidator türünde değil ise
-				throw new Exception(AspectMessages.WrongValidationType);
+				throw new System.Exception(AspectMessages.WrongValidationType);
 			}
 			//öyleyse atamasını yapabilirim
 			_validatorType = validatorType;
--- Core/Utilities/Interceptors/AspectInterceptorSelector.cs
+++ Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -1,4 +1,6 @@
 ﻿using Castle.DynamicProxy;
+using Core.Aspects.Autofac.Exception;
+using Core.CrossCuttingConcerns.Logging.Log4Net.Loggers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,8 @@ namespace Core.Utilities.Interceptors
 				.ToList();
 			var methodAttributes = type.GetMethod(method.Name).GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
 			classAttributes.AddRange(methodAttributes);
+			
+			classAttributes.Add(new ExceptionLogAspect(typeof(FileLogger))); //ExceptionLogAspect için eklendi
 
 			return classAttributes.OrderBy(x => x.Priority).ToArray();
 		}
