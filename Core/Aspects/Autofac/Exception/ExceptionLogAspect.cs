3a7574e087b60d40966e01c156fcc7ecee946f09
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-01-27 15:35:25 +0300

    NorthwindApp #62

---
+++ Core/Aspects/Autofac/Exception/ExceptionLogAspect.cs
@@ -0,0 +1,31 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Logging.Log4Net;
+using Core.Messages;
+using Core.Utilities.Interceptors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Aspects.Autofac.Exception
+{
+	public class ExceptionLogAspect:MethodInterception
+	{
+		private LoggerServiceBase _loggerServiceBase;
+
+		public ExceptionLogAspect(Type loggerService)
+		{
+			//DatabaseLogger veya FileLogger değil ise
+			if (loggerService.BaseType != typeof(LoggerServiceBase))
+			{
+				throw new System.Exception(AspectMessages.WrongLoggerType);
+			}
+
+			_loggerServiceBase = (LoggerServiceBase)Activator.CreateInstance(loggerService);
+		}
+
+		protected override void OnException(IInvocation ınvocation)
+		{
+			base.OnException(ınvocation);
+		}
+	}
+}
